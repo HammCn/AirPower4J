33d3eb326ebe6711f1051453af99b02da0ea797d
Hamm <majhamm@qq.com>
2023-08-15 16:48:21 +0800

    feat(CookieUtil): 提供了Cookie的一些操作和配置

---
+++ src/main/java/cn/hamm/airpower/config/CookieConfig.java
@@ -0,0 +1,37 @@
+package cn.hamm.airpower.config;
+
+/**
+ * @author Hamm https://hamm.cn
+ */
+public class CookieConfig {
+
+    /**
+     * <h1>Cookie的域名</h1>
+     */
+    public static String cookieDomain = "localhost";
+
+    /**
+     * <h1>Cookie的路径</h1>
+     */
+    public static String cookiePath = "/";
+
+    /**
+     * <h1>身份验证的Cookie名称</h1>
+     */
+    public static String authCookieName = "authorization-key";
+
+    /**
+     * <h1>Cookie的HttpOnly配置</h1>
+     */
+    public static boolean isCookieHttpOnly = true;
+
+    /**
+     * <h1>Cookie有效期</h1>
+     */
+    public static int cookieMaxAge = 86400;
+
+    /**
+     * <h1>使用Https方式的安全Cookie</h1>
+     */
+    public static boolean isCookieSecurity = false;
+}
---
+++ src/main/java/cn/hamm/airpower/security/CookieUtil.java
@@ -0,0 +1,39 @@
+package cn.hamm.airpower.security;
+
+import cn.hamm.airpower.config.CookieConfig;
+
+import javax.servlet.http.Cookie;
+
+/**
+ * <h1>Cookie助手</h1>
+ *
+ * @author Hamm
+ */
+public class CookieUtil {
+    /**
+     * <h1>获取一个Cookie</h1>
+     *
+     * @param key   Key
+     * @param value Value
+     * @return Cookie
+     */
+    public static Cookie getCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setDomain(CookieConfig.cookieDomain);
+        cookie.setHttpOnly(CookieConfig.isCookieHttpOnly);
+        cookie.setMaxAge(CookieConfig.cookieMaxAge);
+        cookie.setSecure(CookieConfig.isCookieSecurity);
+        cookie.setPath(CookieConfig.cookiePath);
+        return cookie;
+    }
+
+    /**
+     * <h1>获取一个身份验证的Cookie</h1>
+     *
+     * @param value 身份串的值
+     * @return Cookie
+     */
+    public static Cookie getAuthorizeCookie(String value) {
+        return getCookie(CookieConfig.authCookieName, value);
+    }
+}
