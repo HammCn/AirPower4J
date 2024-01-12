63adc85e4b1cf59282fc0d01e66ddbc62415a4b3
Hamm <majhamm@qq.com>
2024-01-12 12:50:30 +0800

    fix(Service): 修复了`beforeGetPage`错误的类型限定

--- airpower-core/src/main/java/cn/hamm/airpower/root/RootService.java
+++ airpower-core/src/main/java/cn/hamm/airpower/root/RootService.java
@@ -163,7 +163,7 @@ public class RootService<E extends RootEntity<E>, R extends RootRepository<E>> {
      *
      * @param queryPageRequest 请求的request对象
      * @return 分页查询列表
-     * @see #beforeGetPage(QueryRequest)
+     * @see #beforeGetPage(QueryPageRequest) 
      * @see #afterGetPage(QueryPageResponse)
      */
     public final QueryPageResponse<E> getPage(QueryPageRequest<E> queryPageRequest) {
@@ -187,7 +187,7 @@ public class RootService<E extends RootEntity<E>, R extends RootRepository<E>> {
      * @return 处理后的请求数据
      * @see #getPage(QueryPageRequest)
      */
-    protected <T extends QueryRequest<E>> T beforeGetPage(T sourceRequestData) {
+    protected <T extends QueryPageRequest<E>> T beforeGetPage(T sourceRequestData) {
         return sourceRequestData;
     }
 
