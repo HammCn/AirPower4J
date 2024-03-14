6d6e0c62cf51cdfe912bf6ec6f2b7bb630f0beff
Hamm <majhamm@qq.com>
2024-03-14 18:43:28 +0800

    fix(Dictionary): 优化字典校验的一些业务逻辑

---
+++ airpower-core/src/test/java/cn/hamm/airpower/AirPowerTests.java
@@ -0,0 +1,11 @@
+package cn.hamm.airpower;
+
+import org.junit.jupiter.api.Test;
+
+
+public class AirPowerTests {
+    @Test
+    void init() {
+        System.out.println("Hello AirPower Tests");
+    }
+}
