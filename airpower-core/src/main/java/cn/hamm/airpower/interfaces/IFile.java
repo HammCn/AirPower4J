edddcc3fad57bb503dc03d5d184c2f54259fda38
Hamm <majhamm@qq.com>
2024-03-29 13:08:35 +0800

    feat(IFile): 提供了一个标准的文件实体接口

---
+++ airpower-core/src/main/java/cn/hamm/airpower/interfaces/IFile.java
@@ -0,0 +1,23 @@
+package cn.hamm.airpower.interfaces;
+
+/**
+ * <h1>标准文件接口</h1>
+ *
+ * @author Hamm
+ */
+public interface IFile<E extends IFile<E>> extends IEntity<E> {
+    /**
+     * <h2>获取文件的URL</h2>
+     *
+     * @return URL
+     */
+    String getUrl();
+
+    /**
+     * <h2>设置文件的URL</h2>
+     *
+     * @param id URL
+     * @return 实体
+     */
+    E getUrl(Long id);
+}
