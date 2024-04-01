8af77432a35e4c5c2ab8bdd0ecd4490af1c6ae97
BeauHou <i@beauhou.cn>
2024-04-01 21:36:43 +0800

    feat:自动引入cn.hamm.airpower下的bean

--- airpower-core/src/main/java/cn/hamm/airpower/config/CoreAutoConfiguration.java
+++ airpower-core/src/main/java/cn/hamm/airpower/config/CoreAutoConfiguration.java
@@ -1,12 +1,14 @@
 package cn.hamm.airpower.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 自动装配类
+ * @author BeauHou
  */
 @AutoConfiguration
+@ComponentScan("cn.hamm.airpower")
 public class CoreAutoConfiguration {
 
 }
