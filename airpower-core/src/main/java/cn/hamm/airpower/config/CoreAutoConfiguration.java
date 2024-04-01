16b9dd257cef960eb17b6dc9f50098202cf69325
BeauHou <i@beauhou.cn>
2024-04-01 21:03:01 +0800

    feat:core模块新增自动装配类

---
+++ airpower-core/src/main/java/cn/hamm/airpower/config/CoreAutoConfiguration.java
@@ -0,0 +1,12 @@
+package cn.hamm.airpower.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 自动装配类
+ */
+@AutoConfiguration
+public class CoreAutoConfiguration {
+
+}
