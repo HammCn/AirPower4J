a11305e6341ef2e85fe732d46e2487840d0653ba
Hamm <majhamm@qq.com>
2024-04-16 14:40:51 +0800

    fix(BUG): 修复了一些已知的问题

--- airpower-core/src/main/java/cn/hamm/airpower/root/NullEntity.java
+++ airpower-core/src/main/java/cn/hamm/airpower/root/NullEntity.java
@@ -1,14 +1,21 @@
 package cn.hamm.airpower.root;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import cn.hamm.airpower.interfaces.IEntity;
 
 /**
  * <h1>空实体</h1>
  *
  * @author Hamm
  */
-@EqualsAndHashCode(callSuper = true)
-@Data
 public class NullEntity extends RootEntity<NullEntity> {
+    /**
+     * <h2>创建一个空实体</h2>
+     *
+     * @param <T> 实体类型
+     * @return 空实体
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends IEntity<T>> T createNull(Class<T> clazz) {
+        return (T) new NullEntity();
+    }
 }
