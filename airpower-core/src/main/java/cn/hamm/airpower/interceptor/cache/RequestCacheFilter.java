d33b0c68afc5a641ea7ba048a3522e01b3b80b2f
Hamm <majhamm@qq.com>
2024-04-25 15:16:40 +0800

    fix(BUG): 修复了一些已知的问题

--- airpower-core/src/main/java/cn/hamm/airpower/interceptor/cache/RequestCacheFilter.java
+++ airpower-core/src/main/java/cn/hamm/airpower/interceptor/cache/RequestCacheFilter.java
@@ -4,6 +4,9 @@ import cn.hamm.airpower.request.RequestUtil;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,17 +18,32 @@ import java.io.IOException;
  */
 @Component
 @WebFilter
+@Slf4j
 public class RequestCacheFilter implements Filter {
     @Override
     public final void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
-        // 如果是上传 不做任何缓存
-        if (RequestUtil.isUploadRequest(servletRequest)) {
+        HttpServletRequest request = ((HttpServletRequest) servletRequest);
+        // 如果是GET请求 不做任何缓存
+        if (HttpMethod.GET.name().equalsIgnoreCase(request.getMethod())) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        RequestBodyCacheWrapper wrapper = new RequestBodyCacheWrapper(httpServletRequest);
-        filterChain.doFilter(wrapper, servletResponse);
+        if (!request.getContentType().contains(MediaType.APPLICATION_JSON_VALUE)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        try {
+            // 如果是上传 不做任何缓存
+            if (RequestUtil.isUploadRequest(servletRequest)) {
+                filterChain.doFilter(servletRequest, servletResponse);
+                return;
+            }
+            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+            RequestBodyCacheWrapper wrapper = new RequestBodyCacheWrapper(httpServletRequest);
+            filterChain.doFilter(wrapper, servletResponse);
+        } catch (Exception exception) {
+            log.error("缓存请求发生异常", exception);
+        }
     }
 }
