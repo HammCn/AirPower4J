42d21cd4aeb26d7f9ce7b6291752fe2884efa6f4
Hamm <majhamm@qq.com>
2024-05-13 00:49:01 +0800

    fix(BUG): 修复了一些已知问题
    Signed-off-by: Hamm <majhamm@qq.com>

--- airpower-core/src/test/java/cn/hamm/airpower/AirPowerTests.java
+++ airpower-core/src/test/java/cn/hamm/airpower/AirPowerTests.java
@@ -1,11 +1,32 @@
 package cn.hamm.airpower;
 
+import cn.hamm.airpower.model.Page;
+import cn.hamm.airpower.util.HttpUtil;
 import org.junit.jupiter.api.Test;
+
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class AirPowerTests {
     @Test
     void init() {
         System.out.println("Hello AirPower Tests");
+    }
+
+    @Test
+    void request() {
+        var request = new HttpUtil();
+        request.setUrl("https://api.hamm.cn/test/http");
+        Page page = new Page();
+//        HttpResponse<String> form = request.setContentType(ContentType.FORM_URLENCODED).post("a=1&b=2");
+//        System.out.println(form.body());
+//        HttpResponse<String> json = request.setContentType(ContentType.JSON).post(Json.toString(page));
+//        System.out.println(json.body());
+        Map<String, Object> cookies = new HashMap<>();
+        cookies.put("a", "1");
+        HttpResponse<String> cookie = request.setCookies(cookies).addHeader("who", "Hamm").send();
+        System.out.println(cookie.body());
     }
 }
