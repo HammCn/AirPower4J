70bdc2968d2eebfd71b24b9e7e500ea35830d0b0
Hamm <majhamm@qq.com>
2024-05-22 00:21:18 +0800

    fix(BUG): 修复了一些已知BUG
    Signed-off-by: Hamm <majhamm@qq.com>

--- airpower-core/src/main/java/cn/hamm/airpower/util/DictionaryUtil.java
+++ airpower-core/src/main/java/cn/hamm/airpower/util/DictionaryUtil.java
@@ -1,12 +1,13 @@
 package cn.hamm.airpower.util;
 
-import cn.hamm.airpower.config.Constant;
 import cn.hamm.airpower.interfaces.IDictionary;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,32 +53,46 @@ public class DictionaryUtil {
      * @return 返回结果
      */
     public final <D extends IDictionary> @NotNull List<Map<String, Object>> getDictionaryList(@NotNull Class<D> clazz) {
-        return getDictionaryList(clazz, IDictionary::getKey, IDictionary::getLabel);
+        return getDictionaryList(clazz, "key", "label");
     }
 
     /**
      * <h2>获取指定枚举类的Map数据</h2>
      *
-     * @param clazz     枚举类
-     * @param functions 枚举类中的方法
+     * @param clazz  枚举类
+     * @param params 参数列表
      * @return 返回结果
      */
-    @SafeVarargs
     public final <D extends IDictionary> @NotNull List<Map<String, Object>> getDictionaryList(
-            @NotNull Class<D> clazz, Function<D, Object>... functions
+            @NotNull Class<D> clazz, String... params
     ) {
         List<Map<String, Object>> mapList = new ArrayList<>();
-        for (D d : clazz.getEnumConstants()) {
+        for (Object obj : clazz.getEnumConstants()) {
             //取出所有枚举类型
-            Map<String, Object> item = new HashMap<>(functions.length);
-            for (Function<D, Object> function : functions) {
-                item.put(function.toString().replace(Constant.GET, Constant.EMPTY_STRING).toLowerCase(), function.apply(d));
+            Map<String, Object> item = new HashMap<>(params.length);
+            for (String param : params) {
+                // 依次取出参数的值
+                try {
+                    Method method = clazz.getMethod("get" + StringUtils.capitalize(param));
+                    item.put(param, method.invoke(obj).toString());
+                } catch (Exception exception) {
+                    log.error(exception.getMessage(), exception);
+                }
             }
             mapList.add(item);
         }
         return mapList;
     }
 
+    /**
+     * <h2>根据字典类和函数获取枚举字典</h2>
+     *
+     * @param enumClass 字典类
+     * @param function  函数
+     * @param value     值
+     * @param <D>       字典类
+     * @return 字典
+     */
     public <D extends IDictionary> D getDictionary(@NotNull Class<D> enumClass, Function<D, Object> function, Object value) {
         try {
             // 取出所有枚举类型
