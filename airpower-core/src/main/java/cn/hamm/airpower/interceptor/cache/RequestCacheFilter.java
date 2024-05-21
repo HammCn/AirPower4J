fb5d19283d09dba227cd5ce6fe3cbd7e637f7c5c
Hamm <majhamm@qq.com>
2024-05-21 13:20:32 +0800

    fix(Cache): 修复缓存请求时`content-type`可能为空的BUG

--- airpower-core/src/main/java/cn/hamm/airpower/interceptor/cache/RequestCacheFilter.java
+++ airpower-core/src/main/java/cn/hamm/airpower/interceptor/cache/RequestCacheFilter.java
@@ -1,15 +1,19 @@
 package cn.hamm.airpower.interceptor.cache;
 
 import cn.hamm.airpower.util.AirUtil;
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.util.Objects;
 
 /**
  * <h1>缓存请求的过滤器</h1>
@@ -21,29 +25,39 @@ import java.io.IOException;
 @Slf4j
 public class RequestCacheFilter implements Filter {
     @Override
-    public final void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
+    public final void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest request = ((HttpServletRequest) servletRequest);
-        // 如果是GET请求 不做任何缓存
-        if (HttpMethod.GET.name().equalsIgnoreCase(request.getMethod())) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-        if (!request.getContentType().contains(MediaType.APPLICATION_JSON_VALUE)) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
         try {
             // 如果是上传 不做任何缓存
-            if (AirUtil.getRequestUtil().isUploadRequest(servletRequest)) {
+            if (!requestCacheRequired(request)) {
                 filterChain.doFilter(servletRequest, servletResponse);
                 return;
             }
-            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-            RequestBodyCacheWrapper wrapper = new RequestBodyCacheWrapper(httpServletRequest);
+            RequestBodyCacheWrapper wrapper = new RequestBodyCacheWrapper(request);
             filterChain.doFilter(wrapper, servletResponse);
         } catch (Exception exception) {
             log.error(exception.getMessage(), exception);
         }
+    }
+
+    /**
+     * 判断是否需要缓存
+     *
+     * @param request 请求
+     * @return 是否需要缓存
+     */
+    private boolean requestCacheRequired(@NotNull HttpServletRequest request) {
+        // GET请求不缓存
+        if (HttpMethod.GET.name().equalsIgnoreCase(request.getMethod())) {
+            return false;
+        }
+
+        // 空ContentType或者非JSON不缓存
+        String contentType = request.getContentType();
+        if (Objects.isNull(contentType) || !contentType.contains(MediaType.APPLICATION_JSON_VALUE)) {
+            return false;
+        }
+        // 上传请求不缓存
+        return !AirUtil.getRequestUtil().isUploadRequest(request);
     }
 }
