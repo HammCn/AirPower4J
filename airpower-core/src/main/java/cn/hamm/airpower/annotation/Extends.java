54ffe9aa9f4c0aedd19f9970bde3975eeb926533
Hamm <majhamm@qq.com>
2024-09-03 16:19:24 +0800

    style(Core): 文档更新和字段注解增强

--- airpower-core/src/main/java/cn/hamm/airpower/annotation/Extends.java
+++ airpower-core/src/main/java/cn/hamm/airpower/annotation/Extends.java
@@ -8,7 +8,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <h1>继承的接口(白名单优先)</h1>
+ * <h1>继承的接口 {@code 白名单优先}</h1>
  *
  * <li>如不标记此注解,则默认将所有基类接口继承</li>
  * <li>如标记白名单,则需写全需要继承的接口</li>
