af4c1e9a308d67a4e01b0560afce6e5a04616fd6
Hamm <majhamm@qq.com>
2025-02-20 14:45:57 +0800

    refactor(core):重构 OpenApiAspect 类
    -优化了 OpenApi 切面的逻辑结构
    - 新增 validOpenApi 和 getOpenRequest 方法，提高代码复用性
    - 改进了异常处理，增加了专门的 updateLogResponse 方法
    - 删除了冗余的空方法 pointCut
    
    Signed-off-by: Hamm <majhamm@qq.com>

--- airpower-core/src/main/java/cn/hamm/airpower/open/OpenApiAspect.java
+++ airpower-core/src/main/java/cn/hamm/airpower/open/OpenApiAspect.java
@@ -33,17 +33,26 @@ public class OpenApiAspect<S extends IOpenAppService, LS extends IOpenLogService
     @Autowired(required = false)
     private LS openLogService;
 
-    @SuppressWarnings("EmptyMethod")
-    @Pointcut("@annotation(cn.hamm.airpower.open.OpenApi)")
-    public void pointCut() {
-
+    /**
+     * <h3>验证切面点是否支持OpenApi</h3>
+     *
+     * @param proceedingJoinPoint {@code ProceedingJoinPoint}
+     */
+    private static void validOpenApi(@NotNull ProceedingJoinPoint proceedingJoinPoint) {
+        Signature signature = proceedingJoinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        Method method = methodSignature.getMethod();
+        OpenApi openApi = method.getAnnotation(OpenApi.class);
+        ServiceError.API_SERVICE_UNSUPPORTED.whenNull(openApi);
     }
 
     /**
-     * <h3>{@code OpenApi}</h3>
+     * <h3>获取OpenApi请求参数</h3>
+     *
+     * @param proceedingJoinPoint {@code ProceedingJoinPoint}
+     * @return {@code OpenRequest}
      */
-    @Around("pointCut()")
-    public Object openApi(@NotNull ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    private static @NotNull OpenRequest getOpenRequest(@NotNull ProceedingJoinPoint proceedingJoinPoint) {
         Object[] args = proceedingJoinPoint.getArgs();
         if (args.length != 1) {
             throw new ServiceException("OpenApi必须接收一个参数");
@@ -51,17 +60,26 @@ public class OpenApiAspect<S extends IOpenAppService, LS extends IOpenLogService
         if (!(args[0] instanceof OpenRequest openRequest)) {
             throw new ServiceException("OpenApi必须接收一个OpenRequest参数");
         }
-        Signature signature = proceedingJoinPoint.getSignature();
-        MethodSignature methodSignature = (MethodSignature) signature;
-        Method method = methodSignature.getMethod();
-        OpenApi openApi = method.getAnnotation(OpenApi.class);
-        ServiceError.API_SERVICE_UNSUPPORTED.whenNull(openApi);
+        return openRequest;
+    }
+
+    @SuppressWarnings("EmptyMethod")
+    @Pointcut("@annotation(cn.hamm.airpower.open.OpenApi)")
+    public void pointCut() {
+
+    }
+
+    /**
+     * <h3>{@code OpenApi} 切面</h3>
+     */
+    @Around("pointCut()")
+    public Object openApi(@NotNull ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        validOpenApi(proceedingJoinPoint);
+        OpenRequest openRequest = getOpenRequest(proceedingJoinPoint);
         Long openLogId = null;
         String response = "";
         try {
-            IOpenApp openApp = getOpenAppFromRequest(openRequest);
-            openRequest.setOpenApp(openApp);
-            openRequest.check();
+            validOpenRequest(openRequest);
             Object object = proceedingJoinPoint.proceed();
             openLogId = addOpenLog(
                     openRequest.getOpenApp(),
@@ -77,31 +95,27 @@ public class OpenApiAspect<S extends IOpenAppService, LS extends IOpenLogService
             updateLogResponse(openLogId, response);
             return object;
         } catch (ServiceException serviceException) {
-            response = Json.toString(Json.create()
-                    .setCode(serviceException.getCode())
-                    .setMessage(serviceException.getMessage())
-            );
-            updateLogResponse(openLogId, response);
+            updateLogResponse(openLogId, serviceException);
             throw serviceException;
         } catch (Exception exception) {
-            updateExceptionResponse(openLogId, exception);
+            updateLogResponse(openLogId, exception);
             throw exception;
         }
     }
 
     /**
-     * <h3>从请求对象中获取 {@code OpenApp}</h3>
+     * <h3>验证请求</h3>
      *
      * @param openRequest {@code OpenRequest}
-     * @return {@code OpenApp}
      */
-    private @NotNull IOpenApp getOpenAppFromRequest(@NotNull OpenRequest openRequest) {
+    private void validOpenRequest(@NotNull OpenRequest openRequest) {
         ServiceError.INVALID_APP_KEY.when(!StringUtils.hasText(openRequest.getAppKey()));
         ServiceError.SERVICE_ERROR.whenNull(openAppService, "注入OpenAppService失败");
         IOpenApp openApp = openAppService.getByAppKey(openRequest.getAppKey());
         ServiceError.INVALID_APP_KEY.whenNull(openApp);
         ServiceError.FORBIDDEN_OPEN_APP_DISABLED.when(openApp.getIsDisabled());
-        return openApp;
+        openRequest.setOpenApp(openApp);
+        openRequest.check();
     }
 
     /**
@@ -135,13 +149,23 @@ public class OpenApiAspect<S extends IOpenAppService, LS extends IOpenLogService
     /**
      * <h3>更新日志异常</h3>
      *
+     * @param openLogId        日志 {@code ID}
+     * @param serviceException 异常
+     */
+    private void updateLogResponse(Long openLogId, @NotNull ServiceException serviceException) {
+        updateLogResponse(openLogId, Json.toString(Json.create()
+                .setCode(serviceException.getCode())
+                .setMessage(serviceException.getMessage())
+        ));
+    }
+
+    /**
+     * <h3>更新日志异常</h3>
+     *
      * @param openLogId 日志 {@code ID}
      * @param exception 异常
      */
-    private void updateExceptionResponse(Long openLogId, Exception exception) {
-        if (Objects.isNull(openLogId)) {
-            return;
-        }
+    private void updateLogResponse(Long openLogId, @NotNull Exception exception) {
         updateLogResponse(openLogId, Json.toString(Json.create().setMessage(exception.getMessage())));
     }
 }
