3ab0643e274f9b832751830f15d04c29aaf7b938
Hamm <majhamm@qq.com>
2025-02-27 14:58:45 +0800

    feat(FileUtil): 添加文件大小格式化功能并优化类结构
    - 新增 formatSize 方法，用于格式化文件大小
    - 添加必要的 imports 和日志注解
    - 更新类文档注释

--- airpower-core/src/main/java/cn/hamm/airpower/util/FileUtil.java
+++ airpower-core/src/main/java/cn/hamm/airpower/util/FileUtil.java
@@ -1,21 +1,48 @@
 package cn.hamm.airpower.util;
 
 import cn.hamm.airpower.config.Constant;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
 
 /**
  * <h1>文件工具类</h1>
  *
  * @author Hamm.cn
  */
+@Slf4j
 public class FileUtil {
     /**
-     * 获取文件名后缀
+     * <h3>获取文件名后缀</h3>
      *
      * @param fileName 文件名
      * @return 后缀
      */
     public static @NotNull String getExtension(@NotNull String fileName) {
         return fileName.substring(fileName.lastIndexOf(Constant.DOT) + 1).toLowerCase();
+    }
+
+    /**
+     * <h3>格式化文件大小</h3>
+     *
+     * @param size 文件大小
+     * @return 格式化后的文件大小
+     */
+    public static String formatSize(long size) {
+        double fileSize = size;
+        if (fileSize <= 0) {
+            log.error("错误的文件大小: {}", size);
+            return Constant.LINE;
+        }
+        String[] units = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        for (String unit : units) {
+            if (fileSize < Constant.FILE_SCALE) {
+                return decimalFormat.format(fileSize) + unit;
+            }
+            fileSize /= Constant.FILE_SCALE;
+        }
+        return Constant.LINE;
     }
 }
