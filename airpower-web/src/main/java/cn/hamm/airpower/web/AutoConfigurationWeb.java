edb1ca71eb7166fc7f0fb71ff85b0849b118bd72
Hamm <majhamm@qq.com>
2025-03-20 21:18:18 +0800

    feat(airpower-web): 添加 Web 模块自动配置
    - 新增 AutoConfigurationWeb 类实现 Web模块自动配置
    - 添加 airpower-web 模块的 Maven项目对象模型文件
    - 创建 Web 模块的自动配置导入文件

---
+++ airpower-web/src/main/java/cn/hamm/airpower/web/AutoConfigurationWeb.java
@@ -0,0 +1,15 @@
+package cn.hamm.airpower.web;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * <h1>自动装配类</h1>
+ *
+ * @author Hamm.cn
+ */
+@AutoConfiguration
+@ComponentScan("cn.hamm.airpower.web")
+public class AutoConfigurationWeb {
+
+}
